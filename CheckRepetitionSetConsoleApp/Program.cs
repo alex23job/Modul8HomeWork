37fa4a689dc413529b59bc88ed5b4872e199047e
alex23job <alex23job@yandex.ru>
2023-09-30 00:18:43 +0300

    add HashSet for test numbers

---
+++ CheckRepetitionSetConsoleApp/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckRepetitionSetConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Проверка введённых чисел с коллекцией HashSet\n");
+            string strInput = "";
+            HashSet<int> mySet = new HashSet<int>();
+            while(true)
+            {
+                Console.WriteLine("Для добавления числа в базу введите < 1 >");
+                Console.WriteLine("Для просмотра всех чисел введите < 2 >");
+                Console.WriteLine("Для проверки числа введите < 3 >");
+                Console.WriteLine("Для добавления 10 случайных чисел в базу (от 1 до 100) введите < 4 >");
+                Console.WriteLine("Для завершения работы введите < 0 >");
+                strInput = Console.ReadLine();
+                switch(strInput)
+                {
+                    case "1":
+                        AddNumber(mySet);
+                        break;
+                    case "2":
+                        PrintSet(mySet);
+                        break;
+                    case "3":
+                        TestNumber(mySet);
+                        break;
+                    case "4":
+                        Generate10Numbers(mySet);
+                        break;
+                    default:
+                        break;
+                }
+                if (strInput == "0") break;
+            }
+        }
+
+        private static void Generate10Numbers(HashSet<int> mySet)
+        {
+            int i = 0, zn;
+            Random rnd = new Random();
+            while (i < 10) 
+            {
+                zn = rnd.Next(1, 101);
+                if (mySet.Contains(zn)) continue;
+                else
+                {
+                    mySet.Add(zn);
+                    i++;
+                }
+            }
+        }
+
+        private static void AddNumber(HashSet<int> mySet)
+        {
+            string strInput;
+            while(true)
+            {
+                Console.WriteLine("Введите число : ");
+                strInput = Console.ReadLine();
+                if (int.TryParse(strInput, out int zn))
+                {
+                    mySet.Add(zn);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка ввода числа : < {strInput} >");
+                }
+            }
+        }
+
+        private static void PrintSet(HashSet<int> mySet)
+        {
+            int i = 0;
+            foreach (int zn in mySet)
+            {
+                if ((i + 1) % 10 == 0)
+                {
+                    Console.Write($"{zn, 4}\n");
+                }
+                else
+                {
+                    Console.Write($"{zn, 4}");
+                }
+                i++;
+            }
+            Console.WriteLine();
+        }
+
+        private static void TestNumber(HashSet<int> mySet)
+        {
+            string strInput;
+            while (true)
+            {
+                Console.WriteLine("Введите число  ( для выхода - ENTER ) : ");
+                strInput = Console.ReadLine();
+                if (strInput == "") break;
+                if (int.TryParse(strInput, out int zn))
+                {
+                    if (mySet.Contains(zn))
+                    {
+                        Console.WriteLine($"Число {zn} есть в базе");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Числа {zn} нет в базе");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка ввода числа : < {strInput} >");
+                }
+            }
+        }
+    }
+}
---
+++ CheckRepetitionSetConsoleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("CheckRepetitionSetConsoleApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CheckRepetitionSetConsoleApp")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("90124bb7-0797-4e13-bbe4-7429bff1a084")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
